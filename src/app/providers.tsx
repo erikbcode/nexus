3fc3f80a172367e0bdd53ccf319f3ee163cc640e
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-18 03:47:36 -0500

    Removed ClerkProvider from Providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "next-themes";
-import React, { useState, useEffect } from "react";
+import { ClerkProvider } from '@clerk/nextjs';
+import { ThemeProvider } from 'next-themes';
+import React, { useEffect, useState } from 'react';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const [mounted, setMounted] = useState(false);
-
+  // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
     setMounted(true);
   }, []);
@@ -15,11 +15,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
     return <>{children}</>;
   }
 
-  return (
-    <ClerkProvider>
-      <ThemeProvider attribute="class">{children}</ThemeProvider>;
-    </ClerkProvider>
-  );
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
 };
 
 export default Providers;
