75c040ec26ab77d0aa44449013b7d33279734117
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-18 03:47:24 -0500

    Removed ThemeSwitcher from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import ThemeSwitcher from "@/components/ThemeSwitcher";
-import Image from "next/image";
+import ThemeSwitcher from '@/app/components/ThemeSwitcher';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <span className="text-red-500 dark:text-blue-300">text</span>
-      <ThemeSwitcher />
     </div>
   );
 }
