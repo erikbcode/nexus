f70ecc99b257bcd5d835c7fc83dab10fec6dfc55
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-21 01:42:53 -0500

    Now using shadcn Dropdown instead of nextui

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,9 +1,17 @@
 'use client';
 import React from 'react';
 import { User } from 'next-auth';
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem, Avatar } from '@nextui-org/react';
 import { signOut } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/DropdownMenu';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/Avatar';
 
 interface UserAccountNavProps {
   user: Pick<User, 'id' | 'name' | 'image'>;
@@ -12,30 +20,23 @@ interface UserAccountNavProps {
 const UserAccountNav = ({ user }: UserAccountNavProps) => {
   const router = useRouter();
   return (
-    <Dropdown>
-      <DropdownTrigger>
-        <Avatar
-          isBordered
-          as="button"
-          className="transition-transform"
-          size="md"
-          src={user.image!}
-          fallback="no image"
-        />
-      </DropdownTrigger>
-      <DropdownMenu disabledKeys={[]}>
-        <DropdownItem key="profile" className="h-14 gap-2">
-          <p className="font-semibold">Signed in as</p>
-          <p className="font-semibold">{user.name}</p>
-        </DropdownItem>
-        <DropdownItem key="settings" onClick={() => router.push(`/user/${user.id}`)}>
-          My Profile
-        </DropdownItem>
-        <DropdownItem key="logout" color="danger" onClick={() => signOut()}>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={user.image!} alt="userimage" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Signed in as {user.name}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => router.push(`/user/${user.id}`)}>Profile</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="focus:bg-red-400 focus:text-white" onClick={() => signOut()}>
           Log Out
-        </DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
