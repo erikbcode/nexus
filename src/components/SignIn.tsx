d740642b5229014aa7fcda84da6762745a33c4aa
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-21 00:54:19 -0500

    Added SignIn component to be displayed on sign-in page and modals

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button } from './ui/Button';
+import AuthForm from './AuthForm';
+import Link from 'next/link';
+
+const SignIn = () => {
+  return (
+    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
+      <div className="flex flex-col space-y-2 text-center">
+        <div>Logo</div>
+        <div className="text-2xl font-semibold">Welcome Back</div>
+        <div className="text-md max-w-xs mx-auto">
+          By continuing, you are setting up a Nexus account agree to our User Agreement and Privacy Policy.
+        </div>
+        <AuthForm />
+        <p>
+          New to Nexus?{' '}
+          <Link className="underline underline-offset-4" href="/sign-up">
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
