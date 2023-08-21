dd7993029a580b6be538e5db1449c3e26e3593dd
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-21 00:53:36 -0500

    Added AuthForm component that only displays Google right now

---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Button } from './ui/Button';
+import { signIn } from 'next-auth/react';
+import Image from 'next/image';
+import { useToast } from './ui/use-toast';
+
+const AuthForm = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn('google');
+    } catch (error) {
+      toast({
+        title: 'There was a problem.',
+        description: 'There was an error logging in with Google',
+        variant: 'destructive',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex justify-center w-full">
+      <Button
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+        className="flex flex-row w-full gap-2 items-center justify-center"
+      >
+        <Image width="20" height="20" src="/images/google_g_logo.png" alt="Google Logo" />
+        Sign In With Google
+      </Button>
+    </div>
+  );
+};
+
+export default AuthForm;
