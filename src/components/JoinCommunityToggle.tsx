1164dedb92ff79170b0bea6334d6821966196c04
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-22 00:01:24 -0500

    Added JoinCommunityToggle component for leaving / joining

---
+++ src/components/JoinCommunityToggle.tsx
@@ -0,0 +1,136 @@
+'use client';
+import React, { startTransition } from 'react';
+import { Button } from './ui/Button';
+import { useMutation } from '@tanstack/react-query';
+import { NextResponse } from 'next/server';
+import { SubscribeToSubnexusPayload } from '@/lib/validators/subnexus';
+import { toast } from './ui/use-toast';
+import { useRouter } from 'next/navigation';
+
+interface JoinCommunityToggleProps {
+  subnexusId: string;
+  subnexusName: string;
+  isMember: boolean;
+}
+
+const JoinCommunityToggle = ({ subnexusId, subnexusName, isMember }: JoinCommunityToggleProps) => {
+  const router = useRouter();
+
+  const { mutate: joinCommunity, isLoading: isJoinLoading } = useMutation({
+    mutationFn: async () => {
+      const payload: SubscribeToSubnexusPayload = {
+        subnexusId: subnexusId,
+      };
+
+      const response = await fetch('/api/subnexus/subscribe', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw response;
+      }
+
+      const data = await response.json();
+      return data.subnexusId as string;
+    },
+    onError: (err: Response) => {
+      if (err.status === 401) {
+        return toast({
+          title: 'Login required.',
+          description: 'You need to be logged in to do that.',
+          variant: 'destructive',
+        });
+      }
+      if (err.status === 400) {
+        return toast({
+          title: 'Already subscribed.',
+          description: 'You are already subscribed to this community.',
+          variant: 'destructive',
+        });
+      }
+      return toast({
+        title: 'There was an error.',
+        description: 'Please try again later.',
+        variant: 'destructive',
+      });
+    },
+    onSuccess: () => {
+      startTransition(() => {
+        router.refresh();
+      });
+      return toast({
+        title: 'Subscribed!',
+        description: `You are now subscribed to n/${subnexusName}`,
+      });
+    },
+  });
+
+  const { mutate: leaveCommunity, isLoading: isLeaveLoading } = useMutation({
+    mutationFn: async () => {
+      const payload: SubscribeToSubnexusPayload = {
+        subnexusId: subnexusId,
+      };
+
+      const response = await fetch('/api/subnexus/unsubscribe', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw response;
+      }
+
+      const data = await response.json();
+      return data.subnexusId as string;
+    },
+    onError: (err: Response) => {
+      if (err.status === 401) {
+        return toast({
+          title: 'Login required.',
+          description: 'You need to be logged in to do that.',
+          variant: 'destructive',
+        });
+      }
+      if (err.status === 400) {
+        return toast({
+          title: 'Not subscribed.',
+          description: 'You cannot leave a community that you are not susbcribed to.',
+          variant: 'destructive',
+        });
+      }
+      return toast({
+        title: 'There was an error.',
+        description: 'Please try again later.',
+        variant: 'destructive',
+      });
+    },
+    onSuccess: () => {
+      startTransition(() => {
+        router.refresh();
+      });
+      return toast({
+        title: 'Unsubscribed!',
+        description: `You are now unsubscribed from n/${subnexusName}`,
+      });
+    },
+  });
+
+  return isMember ? (
+    <Button onClick={() => leaveCommunity()} disabled={isLeaveLoading}>
+      Leave Community
+    </Button>
+  ) : (
+    <Button onClick={() => joinCommunity()} disabled={isJoinLoading}>
+      Join Community
+    </Button>
+  );
+};
+
+export default JoinCommunityToggle;
