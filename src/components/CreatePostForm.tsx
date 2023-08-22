3ea44a9afb3eec5ffabe24eb84633b2ca7e2d02d
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-22 00:01:02 -0500

    Form to create a post that redirects to /pathname/submit

---
+++ src/components/CreatePostForm.tsx
@@ -0,0 +1,34 @@
+'use client';
+import React, { useState } from 'react';
+import { Textarea } from '@/components/ui/Textarea';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/Avatar';
+import { Session } from 'next-auth';
+import Image from 'next/image';
+import UserAvatar from './ui/UserAvatar';
+import { Input } from './ui/Input';
+import { usePathname, useRouter } from 'next/navigation';
+import { ImageIcon, LinkIcon } from 'lucide-react';
+import { Button } from './ui/Button';
+
+interface CreatePostFormProps {
+  session: Session | null;
+}
+
+const CreatePostForm = ({ session }: CreatePostFormProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  return (
+    <div className="w-full shadow bg-white rounded-md py-6 px-4 flex flex-row gap-2 items-center">
+      <UserAvatar user={{ name: session?.user.name!, image: session?.user.image! }} />
+      <Input className="text-base h-10" onClick={() => router.push(`${pathname}/submit`)} placeholder="Create post" />
+      <Button variant="ghost" onClick={() => router.push(`${pathname}/submit`)}>
+        <ImageIcon />
+      </Button>
+      <Button variant="ghost" onClick={() => router.push(`${pathname}/submit`)}>
+        <LinkIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default CreatePostForm;
