5954ddc46b20accdbff43ef43dc5f9d688a73e39
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-22 01:40:50 -0500

    Added submit page which displays Editor component

--- src/app/n/[name]/submit/page.tsx
+++ src/app/n/[name]/submit/page.tsx
@@ -1,7 +1,23 @@
+import Editor from '@/components/Editor';
+import { Label } from '@/components/ui/Label';
 import React from 'react';
 
-const Page = () => {
-  return <div>Page</div>;
+interface SubmitPageProps {
+  params: {
+    name: string;
+  };
+}
+
+const Page = ({ params }: SubmitPageProps) => {
+  return (
+    <div className="">
+      <div className="flex flex-row items-center mb-8">
+        <h3 className="font-semibold text-lg">Create Post </h3>
+        <p className="ml-2 font-light text-zinc-500"> in n/{params.name}</p>
+      </div>
+      <Editor subnexusName={params.name} />
+    </div>
+  );
 };
 
 export default Page;
