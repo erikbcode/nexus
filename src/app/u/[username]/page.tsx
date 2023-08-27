ffb888bde787063e6a403acba0807040932c9bb0
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-26 20:31:38 -0500

    Added UserPostList component to user profile page

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,3 +1,6 @@
+import UserPostList from '@/components/UserPostList';
+import { fetchUserPosts } from '@/lib/actions/dbActions';
+import { prisma } from '@/lib/db';
 import React from 'react';
 
 interface ProfilePageProps {
@@ -5,8 +8,16 @@ interface ProfilePageProps {
     username: string;
   };
 }
-const Page = ({ params }: ProfilePageProps) => {
-  return <div>Page</div>;
+const Page = async ({ params }: ProfilePageProps) => {
+  const username = params.username;
+
+  const posts = await fetchUserPosts({ username });
+
+  return (
+    <div>
+      <UserPostList initialPosts={posts} username={username} />
+    </div>
+  );
 };
 
 export default Page;
