6782ce7d4606288b50b587a91ba7511ef4559c5d
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-29 21:59:09 -0500

    Added safe-area-inset-top padding to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import ThemeSwitcher from './ui/ThemeSwitcher';
 import { buttonVariants } from './ui/Button';
 import { getAuthSession } from '@/lib/auth';
 import UserAccountNav from './UserAccountNav';
@@ -7,7 +6,7 @@ import UserAccountNav from './UserAccountNav';
 const Navbar = async () => {
   const session = await getAuthSession();
   return (
-    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 dark:bg-zinc-900 dark:border-b dark:border-zinc-800 z-[10] py-2">
+    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 dark:bg-zinc-900 dark:border-b dark:border-zinc-800 z-[10] pb-2 pt-safe-top">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-around xl:justify-between">
         {/* logo */}
         <Link href="/" className="flex gap-2 items-center">
