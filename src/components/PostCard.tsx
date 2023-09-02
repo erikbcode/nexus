f4d58231935d0d3e15b9deadfba2b3a765441998
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-09-01 23:00:16 -0500

    Added section for Comments as a link to the post on PostCard

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -2,37 +2,49 @@
 import { timeSince } from '@/lib/utils';
 import Link from 'next/link';
 import React, { useState } from 'react';
-import { ArrowBigUp, ArrowBigDown, Loader } from 'lucide-react';
-import { Button } from './ui/Button';
-import { VoteType } from '@prisma/client';
-import { updateVote } from '@/lib/actions/posts/actions';
-import { toast } from '@/hooks/use-toast';
+import { ArrowBigUp, ArrowBigDown, Loader, MessageSquare } from 'lucide-react';
+import { Button, buttonVariants } from './ui/Button';
+import { ChatBubbleLeftIcon } from '@heroicons/react/20/solid';
+import { ClientPost } from '@/types/db';
+import { useRouter } from 'next/navigation';
+import VoteDisplay from './VoteDisplay';
+import { ChatBubbleIcon } from '@radix-ui/react-icons';
 
-type Post = {
-  id: string;
-  title: string;
-  content: string;
-  createdAt: Date;
-  voteCount: number;
-  initialVote: VoteType | null;
-  subnexus: {
-    name: string;
-  };
-  author: {
-    id: string;
-    image: string | null;
-    username: string | null;
-  };
-};
+const PostCard = ({
+  id,
+  content,
+  createdAt,
+  title,
+  author,
+  subnexus,
+  voteCount,
+  currentUserVote,
+  commentCount,
+}: ClientPost) => {
+  const router = useRouter();
 
-const PostCard = ({ id, content, createdAt, title, author, subnexus, voteCount, initialVote }: Post) => {
+  const formatNumber = (num: number) => {
+    if (num >= 1000000000) {
+      return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
+    }
+    if (num >= 1000000) {
+      return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+    }
+    if (num >= 1000) {
+      return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+    }
+    return num;
+  };
+
   return (
     <div className="border rounded-lg p-4 hover:shadow-lg transition-shadow duration-200 shadow flex flex-row items-center">
-      <VoteDisplay initialVoteCount={voteCount} initialVote={initialVote} postId={id} />
+      <div className="hidden md:contents">
+        <VoteDisplay initialVoteCount={voteCount} initialVote={currentUserVote} postId={id} />
+      </div>
       <div className="flex flex-col w-full min-w-0">
         {/* Author and Subnexus*/}
         {/* Wide Screen */}
-        <div className="hidden sm:flex items-center mb-2 text-zinc-500">
+        <div className="hidden sm:flex items-center mb-2 text-zinc-500 text-sm md:text-base">
           <Link href={`/n/${subnexus.name}`} className="text-zinc-800 dark:text-white underline-offset-3 underline">
             /n/{subnexus.name}
           </Link>
@@ -46,7 +58,7 @@ const PostCard = ({ id, content, createdAt, title, author, subnexus, voteCount, 
           <span>{timeSince(new Date(createdAt))}</span>
         </div>
         {/* Small Screen */}
-        <div className="flex gap-0 sm:hidden mb-2 text-zinc-500">
+        <div className="flex gap-0 text-sm sm:hidden mb-2 text-zinc-500">
           <Link href={`/n/${subnexus.name}`} className="text-zinc-800 dark:text-white underline-offset-3 underline">
             /n/{subnexus.name}
           </Link>
@@ -62,77 +74,27 @@ const PostCard = ({ id, content, createdAt, title, author, subnexus, voteCount, 
         <h2 className="text-xl font-semibold mb-2">{title}</h2>
 
         {/* Content */}
-        <p className="break-words overflow-wrap text-lg text-gray-700 dark:text-zinc-400 overflow-wrap">{content}</p>
+        <p className="break-words text-lg text-gray-700 dark:text-zinc-400 overflow-wrap">{content}</p>
+
+        <div className="mt-2 flex flex-row">
+          <div className="contents md:hidden">
+            <VoteDisplay postId={id} initialVoteCount={voteCount} initialVote={currentUserVote} smallScreen={true} />
+          </div>
+          <Link
+            className={buttonVariants({
+              variant: 'ghost',
+              className: `text-zinc-400 w-fit flex items-center gap-2 rounded`,
+              size: 'sm',
+            })}
+            href={`/n/${subnexus.name}/post/${id}`}
+          >
+            <ChatBubbleLeftIcon className="w-4 h-4" />
+            <span className="text-xs font-semibold">{formatNumber(commentCount)}</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
 };
 
 export default PostCard;
-
-interface VoteDisplayProps {
-  initialVoteCount: number;
-  initialVote?: VoteType | null;
-  postId: string;
-}
-
-const VoteDisplay = ({ initialVoteCount, initialVote, postId }: VoteDisplayProps) => {
-  const [voteCount, setVoteCount] = useState(initialVoteCount);
-  const [currentVote, setCurrentVote] = useState(initialVote);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleVote = async (voteType: VoteType) => {
-    setIsLoading(true);
-    const res = await updateVote({ data: { voteType, postId } });
-    const variant = res.status === 200 ? 'default' : 'destructive';
-    if (res.status === 200) {
-      // Vote was successful, so update state
-      setCurrentVote(res.data.newVoteType);
-      setVoteCount(voteCount + res.data.updateCount!);
-    }
-
-    setIsLoading(false);
-
-    return toast({
-      title: res.data.title,
-      description: res.data.description,
-      variant,
-    });
-  };
-
-  // Tailwind css classnames for upvote and downvote
-  const upvoteClass =
-    currentVote === VoteType.UP
-      ? 'text-emerald-500 fill-emerald-500 group-hover:fill-none'
-      : 'group-hover:text-emerald-500 dark:text-white';
-  const downvoteClass =
-    currentVote === VoteType.DOWN
-      ? 'text-rose-500 fill-rose-500 group-hover:fill-none'
-      : 'group-hover:text-rose-500 dark:text-white';
-
-  return (
-    <>
-      <div className="px-4 flex flex-col justify-center items-center gap-3">
-        <Button
-          disabled={isLoading}
-          variant="ghost"
-          size="sm"
-          className={`group`}
-          onClick={() => handleVote(VoteType.UP)}
-        >
-          <ArrowBigUp className={`${upvoteClass}`} />
-        </Button>
-        {isLoading ? <Loader className="animate-spin" /> : <p className="font-semibold">{voteCount}</p>}
-        <Button
-          disabled={isLoading}
-          variant="ghost"
-          size="sm"
-          className="dark:text-white group"
-          onClick={() => handleVote(VoteType.DOWN)}
-        >
-          <ArrowBigDown className={`${downvoteClass}`} />
-        </Button>
-      </div>
-    </>
-  );
-};
