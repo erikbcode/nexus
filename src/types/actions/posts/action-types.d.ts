0e178b4be65814a906bf6bae2f226fa26e9592f9
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-09-01 23:37:23 -0500

    Added GetSinglePostResponse type

--- src/types/actions/posts/action-types.d.ts
+++ src/types/actions/posts/action-types.d.ts
@@ -1,5 +1,10 @@
 import { ClientPost } from '@/types/db';
 
+export type GetSinglePostResponse = {
+  status: number;
+  data: ClientPost | null;
+};
+
 export type GetPostsResponse = {
   status: number;
   data: ClientPost[];
