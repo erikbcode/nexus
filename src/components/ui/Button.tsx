b414f1353cb7f4266427cf50140c83dee133f160
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-09-01 23:03:47 -0500

    Modified default padding of buttons

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -12,11 +12,11 @@ export const buttonVariants = cva(
         outline: 'bg-zinc-100 text-zinc-900 hover:bg-zinc-200 outline outline-1 outline-zinc-300',
         subtle: 'hover:bg-zinc-200 bg-zinc-100 text-zinc-900',
         ghost:
-          'bg-transparent hover:bg-zinc-100 dark:hover:bg-zinc-800 text-zinc-800 data-[state=open]:bg-transparent data-[state=open]:bg-transparent',
+          'bg-transparent hover:bg-zinc-100 dark:hover:bg-zinc-800 text-zinc-800 dark:text-zinc-600 data-[state=open]:bg-transparent data-[state=open]:bg-transparent',
         link: 'bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent',
       },
       size: {
-        default: 'h-10 py-2 px-4',
+        default: 'h-10 py-2 px-2',
         sm: 'h-9 px-2 rounded-md',
         xs: 'h-8 px-1.5 rounded-sm',
         lg: 'h-11 px-8 rounded-md',
