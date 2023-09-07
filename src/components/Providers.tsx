f1d41e24596b6f463b40372e251990d537ec4849
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-09-06 22:43:57 -0500

    Added progress bar

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'next-themes';
 import React, { useEffect, useState } from 'react';
 import { SessionProvider } from 'next-auth/react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { Next13ProgressBar } from 'next13-progressbar';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
@@ -11,7 +12,10 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider attribute="class">{children}</ThemeProvider>
+        <ThemeProvider attribute="class">
+          <Next13ProgressBar height="4px" color="#157bdb" options={{ showSpinner: true }} showOnShallow />
+          {children}
+        </ThemeProvider>
       </QueryClientProvider>
     </SessionProvider>
   );
